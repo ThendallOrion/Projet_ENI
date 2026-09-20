46c56843d8dcf61dd4ee84e8b3203b9cd3dcdd7c
ThendallOrion <yannnnlaurent@gmail.com>
2026-09-20 11:41:37 +0200

    refresh ok pour lire la liste des gn

--- SiteInternet/AgesFarouches/src/app/pages/gn/gn.service.ts
+++ SiteInternet/AgesFarouches/src/app/pages/gn/gn.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Gn } from './gn.model';
@@ -11,9 +11,6 @@ export class GnService {
   constructor(private http: HttpClient) {}
 
   getGnList(): Observable<Gn[]> {
-    // Cache-Control: no-cache force le navigateur à revalider/retélécharger
-    // au lieu de servir un 304, le temps d'éliminer le cache comme cause possible.
-    const headers = new HttpHeaders({ 'Cache-Control': 'no-cache' });
-    return this.http.get<Gn[]>(`${environment.apiUrl}/gn`, { headers });
+    return this.http.get<Gn[]>(`${environment.apiUrl}/gn`);
   }
 }
