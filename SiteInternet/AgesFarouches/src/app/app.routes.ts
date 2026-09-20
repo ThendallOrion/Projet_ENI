97fc420aa037a72dd7414fdba68949db4e7c3ba1
ThendallOrion <yannnnlaurent@gmail.com>
2026-09-20 10:13:39 +0200

    lecture 2

--- SiteInternet/AgesFarouches/src/app/app.routes.ts
+++ SiteInternet/AgesFarouches/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
-import { GnListComponent } from './pages/gn/gn';
+import { GnListComponent } from './pages/gn/gn-list';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
