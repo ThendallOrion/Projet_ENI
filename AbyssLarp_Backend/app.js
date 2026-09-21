548eefbae8e4f6093cb92f58d38807bcb2bf88b2
ThendallOrion <yannnnlaurent@gmail.com>
2026-09-21 09:02:39 +0200

    suppresion des routes inutils

--- AbyssLarp_Backend/app.js
+++ AbyssLarp_Backend/app.js
@@ -7,8 +7,6 @@ import cors from 'cors';
 
 import './config/database.js';
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
 import gnRouter from './routes/gn.js';
 import nfcRouter from './routes/nfc.js';
 
@@ -31,8 +29,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/gn', gnRouter);
 app.use("/nfc", nfcRouter);
 
